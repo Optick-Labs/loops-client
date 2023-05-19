7da802b70a62e201ff2332628231f3c67cc7869b
Stefan Mai <stefan.mai@iamnafets.com>
2023-05-19 12:19:20 -0700

    feat: add types for base API calls
    Adds types for the responses of base API calls.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,31 @@
+import { type } from 'os';
+
+type Contact = {
+  id: string;
+  email: string;
+  source: string; // TODO: This is probably a literal union
+  subscribed: boolean;
+  userGroup: string;
+  userId: string | null;
+};
+
 type ContactWithEmail = { email: string } & Record<string, string>;
+
+type AddContactSuccess = { success: true; id: string };
+type AddContactFailure = { success: false; message: string };
+type AddContactResponse = AddContactSuccess | AddContactFailure;
+
+type UpdateConactSuccess = { success: true; id: string };
+type UpdateContactFailure = { success: false; message: string };
+type UpdateContactResponse = UpdateConactSuccess | UpdateContactFailure;
+
+type FindContactResponse = Contact[];
+
+type DeleteContactSuccess = { success: true; message: string };
+type DeleteContactFailure = { success: false; message: string };
+type DeleteContactResponse = DeleteContactSuccess | DeleteContactFailure;
+
+type SendEventResponse = { success: boolean };
 
 export class LoopsClient {
   private apiKey: string;
@@ -16,27 +43,29 @@ export class LoopsClient {
     return headers;
   }
 
-  async addContact(contact: ContactWithEmail) {
+  async addContact(contact: ContactWithEmail): Promise<AddContactResponse> {
     const response = await fetch(`${this.baseUrl}/contacts/create`, {
       method: 'POST',
       headers: this.getHeaders(),
       body: JSON.stringify(contact),
     });
 
-    return response.json();
+    return response.json() as Promise<AddContactResponse>;
   }
 
-  async updateContact(contact: ContactWithEmail) {
+  async updateContact(
+    contact: ContactWithEmail
+  ): Promise<UpdateContactResponse> {
     const response = await fetch(`${this.baseUrl}/contacts/update`, {
       method: 'PUT',
       headers: this.getHeaders(),
       body: JSON.stringify(contact),
     });
 
-    return response.json();
+    return response.json() as Promise<UpdateContactResponse>;
   }
 
-  async findContact(email: string) {
+  async findContact(email: string): Promise<FindContactResponse> {
     const response = await fetch(
       `${this.baseUrl}/contacts/find?email=${encodeURIComponent(email)}`,
       {
@@ -45,30 +74,33 @@ export class LoopsClient {
       }
     );
 
-    return response.json();
+    return response.json() as Promise<FindContactResponse>;
   }
 
-  async deleteContactByEmail(email: string) {
+  async deleteContactByEmail(email: string): Promise<DeleteContactResponse> {
     const response = await fetch(`${this.baseUrl}/contacts/delete`, {
       method: 'DELETE',
       headers: this.getHeaders(),
       body: JSON.stringify({ email }),
     });
 
-    return response.json();
+    return response.json() as Promise<DeleteContactResponse>;
   }
 
-  async deleteContactById(userId: string) {
+  async deleteContactById(userId: string): Promise<DeleteContactResponse> {
     const response = await fetch(`${this.baseUrl}/contacts/delete`, {
       method: 'DELETE',
       headers: this.getHeaders(),
       body: JSON.stringify({ userId }),
     });
 
-    return response.json();
+    return response.json() as Promise<DeleteContactResponse>;
   }
 
-  async sendEvent(eventName: string, contact: ContactWithEmail) {
+  async sendEvent(
+    eventName: string,
+    contact: ContactWithEmail
+  ): Promise<SendEventResponse> {
     const event = {
       ...contact,
       eventName,
@@ -80,7 +112,7 @@ export class LoopsClient {
       body: JSON.stringify(event),
     });
 
-    return response.json();
+    return response.json() as Promise<SendEventResponse>;
   }
 
   async sendTransactionalEmail(
